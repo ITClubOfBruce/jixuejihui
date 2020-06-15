b5f347e104f58babc10af0ad18d1cfc559c4b524
Bruce <1046244623@qq.com>
2020-06-15 15:39:24 +0800

    一级联动，二级联动

--- apps/organization/views.py
+++ apps/organization/views.py
@@ -9,8 +9,18 @@ class OrgView(View):
     '''
     def get(self,request):
         all_orgs = CourseOrg.objects.all()
-        org_onums = all_orgs.count()
         all_citys = CityDict.objects.all()
+
+        # 获取前端传过来的城市id
+        city_id = request.GET.get('city')
+        if city_id:
+            all_orgs = all_orgs.filter(city_id=int(city_id))
+        # 获取到前端传过来的机构类别ct
+        category = request.GET.get('ct')
+        if category:
+            all_orgs = all_orgs.filter(category=category)
+        org_onums = all_orgs.count()
+
         try:
             page = request.GET.get('page',1)
         except PageNotAnInteger:
@@ -23,5 +33,7 @@ class OrgView(View):
             "all_orgs": page_orgs,
             "org_onums": org_onums,
             "all_citys": all_citys,
+            "city_id":city_id,
+            "category":category
         }
         return render(request,'organization/org-list.html',context)
